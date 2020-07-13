18d0823797b97f98da020912701f7735e761f3f1
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-07-13 21:45:33 +1000

    remove source filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,20 +17,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [

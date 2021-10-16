e38de4108db7bb3ae5c06454d57db26fd6b08938
Nicolaus Gozali <nicolauscg@gmail.com>
2021-10-16 20:56:44 +1000

    chore: init nextjs app

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-set -o allexport; source ./.env; set +o allexport
-
-npm run clean && npm run build && \
-  ssh $DEPLOY_BASH_SCRIPT_USERNAME@$DEPLOY_BASH_SCRIPT_HOST rm -rf $DEPLOY_BASH_SCRIPT_PATH/* && \
-  scp -r public/* $DEPLOY_BASH_SCRIPT_USERNAME@$DEPLOY_BASH_SCRIPT_HOST:$DEPLOY_BASH_SCRIPT_PATH

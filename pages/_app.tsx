f968b6b7fc9a1a2d5ab42f23722f6b4682a0ddba
Nicolaus Gozali <nicolauscg@gmail.com>
2021-10-18 18:58:48 +1000

    feat: follow mui nextjs setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 import type { AppProps } from "next/app";
+import createEmotionCache from "../components/createEmotionCache";
+import { NextComponentType, NextPageContext } from "next";
 
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps {
+  Component: NextComponentType<NextPageContext, any, {}>;
+  emotionCache: EmotionCache;
+  pageProps: any;
+}
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
+
 export default MyApp;

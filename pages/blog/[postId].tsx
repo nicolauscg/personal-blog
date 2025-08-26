9da4c02ba6272b36a2fa6a254b3d1bc99ef7af26
Nicolaus C Gozali <nicolauscg@gmail.com>
2025-08-26 21:19:38 +1000

    feat: only render blog posts rendered that can be accessed via official notion API token, set correct header title for blog post index and detail page

--- pages/blog/[postId].tsx
+++ pages/blog/[postId].tsx
@@ -1,22 +1,31 @@
 import { Button } from "@mui/material";
 import Link from "next/link";
-import { getPageContent, queryDatabase } from "../../lib/notionApi";
+import { getPageContent, queryDatabase, getPageProp } from "../../lib/notionApi";
 import { revalidateDurationInSec } from "../../lib/contants";
 import { InferGetStaticPropsType } from "next";
 import { ArrowBack } from "@mui/icons-material";
 import { NotionPage } from "../../components/NotionPage";
+import { getPageTitle } from "notion-utils";
+import Head from "next/head";
 
-export default function BlogPost({ recordMap }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <NotionPage
-    recordMap={recordMap}
-    pageHeader={
-      <Button startIcon={<ArrowBack />}>
-        <Link href="/blog">
-          <span className="normal-case">Back to all posts</span>
-        </Link>
-      </Button>
-    }
-  />
+export default function BlogPost({ recordMap, postTitle }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <Head>
+        <title>nicolauscg | {postTitle}</title>
+      </Head>
+      <NotionPage
+        recordMap={recordMap}
+        pageHeader={
+          <Button startIcon={<ArrowBack />}>
+            <Link href="/blog">
+              <span className="normal-case">Back to all posts</span>
+            </Link>
+          </Button>
+        }
+      />
+    </>
+  )
 }
 
 // TODO use title as slug for better SEO
@@ -48,11 +57,27 @@ export async function getStaticPaths() {
 
 export const getStaticProps = async (context: any) => {
   const postId = context.params?.postId as string;
+
+  // Check if the official Notion API token can access this page id,
+  // this is done to only render pages created by the token's author.
+  try {
+    await getPageProp(postId)
+  } catch (e) {
+    console.error("Failed to get page prop for postId " + postId + ", error: " + e)
+    return {
+      notFound: true,
+      revalidate: revalidateDurationInSec
+    }
+  }
+
+  // Get page content and title using unofficial Notion API client
   const recordMap = await getPageContent(postId)
+  const postTitle = getPageTitle(recordMap) || "blog post";
 
   return {
     props: {
       recordMap,
+      postTitle
     },
     revalidate: revalidateDurationInSec
   }
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,14 +2,22 @@ import { InferGetStaticPropsType } from "next";
 import { revalidateDurationInSec } from "../../lib/contants";
 import { getPageContent } from "../../lib/notionApi";
 import { NotionPage } from "../../components/NotionPage";
+import Head from "next/head";
 
 export default function BlogIndex({
   recordMap,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <NotionPage
-    recordMap={recordMap}
-    className={"blog-index-page"}
-  />
+  return (
+    <>
+      <Head>
+        <title>nicolauscg | blog</title>
+      </Head>
+      <NotionPage
+        recordMap={recordMap}
+        className={"blog-index-page"}
+      />
+    </>
+  );
 }
 
 export const getStaticProps = async () => {
